20541a86f53f1ff28dd6bdf96e424f2300eb4117
HookProcess <hainescole95@gmail.com>
2026-01-08 12:15:51 -0600

    Delete TEST.lua

--- TEST.lua
+++
@@ -1,2 +0,0 @@
-getgenv().SCRIPT_KEY = "KEYLESS"
-loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/ddd9d05ec098f8cb755e0e460ac528bb76a2e0b5005904c207f46aec9dc58c63/download"))()
